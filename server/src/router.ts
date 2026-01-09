1f66c28d49bff6e42379036d9998a4ab05ef8ee9
abijoserah <serahabijo94@gmail.com>
2026-01-09 13:01:31 +0100

    edited the router.ts file to add the post route

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import activityActions from "./modules/activity/activityActions";
 
 const router = express.Router();
 
@@ -6,7 +7,7 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Define item-related routes
+router.post("/api/activity", activityActions.add);
 
 /* ************************************************************************* */
 
