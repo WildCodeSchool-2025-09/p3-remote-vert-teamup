9e06123e3572d13c735a1b3ffe22f5ce464b5b5d
Lisa Bureau <bureau.lisa03@gmail.com>
2026-01-13 17:56:14 +0100

    Update ActivityContext.tsx

--- client/src/context/ActivityContext.tsx
+++ client/src/context/ActivityContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
-import type { Activity } from "../types/Activity";
 
 type ActivityContextType = {
   activities: Activity[];
