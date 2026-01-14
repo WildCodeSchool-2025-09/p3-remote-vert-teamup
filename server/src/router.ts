f246b3060fadf66e5ef4dce81d41b015e2d69589
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-14 15:35:26 +0100

    addition of routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import sportAction from "./modules/sport/sportAction";
+import activityAction from "./modules/activity/activityAction";
 
 const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
+router.get("/api/sport", sportAction.browse);
+router.get("/api/activity", activityAction.browse);
 // Define item-related routes
 
 /* ************************************************************************* */
