3dad538800fcc97b2cadd132682246c1bd31b445
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-20 10:59:28 +0100

    Create placeholder page for MyActivities

---
+++ client/src/pages/MyActivities.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+function MyActivities() {
+  return (
+    <>
+      <h1>Je suis my activities</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default MyActivities;
