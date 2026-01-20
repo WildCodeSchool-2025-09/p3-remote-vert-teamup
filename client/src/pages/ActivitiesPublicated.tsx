7179abed3edb2b9e3ab4dd8477036b6727c0f52a
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-20 10:58:15 +0100

    update naming for ActivitiesPublicated + logic implemented

---
+++ client/src/pages/ActivitiesPublicated.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import ActivityCard from "../components/ActivityCard";
+
+function ActivitiesPublicated() {
+  const [activitiesPublicated, setActivitiesPublicated] = useState<Activity[]>(
+    [],
+  );
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/publication`)
+      .then((response) => response.json())
+      .then((data) => setActivitiesPublicated(data));
+  }, []);
+
+  return (
+    <div className="cards-activity">
+      {activitiesPublicated.map((activityPublicated) => (
+        <ActivityCard
+          activity={activityPublicated}
+          key={activityPublicated.id}
+        />
+      ))}
+    </div>
+  );
+}
+export default ActivitiesPublicated;
