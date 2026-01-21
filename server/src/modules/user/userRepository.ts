4409beb3f0e0eba275bff60dd196d3f4802f0270
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-21 14:09:54 +0100

    Create userRepository.ts

---
+++ server/src/modules/user/userRepository.ts
@@ -0,0 +1,21 @@
+import databaseClient from "../../../database/client";
+import type { Rows } from "../../../database/client";
+
+class ActivityRepository {
+  async readAllParticipants(activityId: number) {
+    const [rows] = await databaseClient.query<Rows>(
+      `SELECT user.username, user.picture, demand.status FROM user 
+      JOIN demand ON user.id = demand.user_id
+      WHERE demand.activity_id = ?
+      UNION
+      SELECT user.username, user.picture, participation.status FROM user 
+      JOIN participation ON user.id = participation.user_id
+      WHERE participation.activity_id = ?`,
+      [activityId, activityId],
+    );
+
+    return rows as Activity[];
+  }
+}
+
+export default new ActivityRepository();
