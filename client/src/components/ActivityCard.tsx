302fc97831ebfe77a7a45974df18faa02610fd7d
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-21 14:08:10 +0100

    Update of ActivityCard, implementation of ParticipationList + update css

--- client/src/components/ActivityCard.tsx
+++ client/src/components/ActivityCard.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "../styles/ActivityCard.css";
+import ParticipantsList from "./ParticipantsList";
 
 type ActivityCardType = {
   activity: Activity;
+  publicatedRoad?: boolean;
 };
 
-function ActivityCard({ activity }: ActivityCardType) {
+function ActivityCard({ activity, publicatedRoad }: ActivityCardType) {
+  const [showParticpants, setShowParticipants] = useState(false);
   const price = Number(activity.price);
   const playing_at = new Date(activity.playing_at);
   const formattedPlayingAt = playing_at.toLocaleDateString("fr-FR", {
@@ -17,93 +21,113 @@ function ActivityCard({ activity }: ActivityCardType) {
   const widthProgressBar = (100 / activity.nb_spots) * activity.nb_participant;
 
   return (
-    <article className="card">
-      <div className={`card-header ${activity.name}`}>
-        <h2>{activity.name}</h2>
-        <p className={`label-price ${price === 0 ? "free" : "paid"}`}>
-          {price === 0
-            ? "Gratuit"
-            : price % 1 === 0
-              ? `${price} €`
-              : `${price.toFixed(2)} €`}
-        </p>
-      </div>
-      <div className="important-info">
-        <img src="/icons/calendar.png" alt="icon-calendar" />
-        <p>
-          {formattedPlayingAt.charAt(0).toUpperCase() +
-            formattedPlayingAt.slice(1)}
-        </p>
-        <img src="/icons/clock.png" alt="icon-clock" />
-        <p>{activity.playing_time.slice(0, 5).replace(":", "h")}</p>
-        <img src="/icons/pin.png" alt="icon-pin" />
-        <p>{activity.city}</p>
-      </div>
-      <div className="card-tags">
-        <p className="card-tag">
-          {activity.level === "all" && "Tous niveaux"}
-          {activity.level === "beginner" && "Débutant"}
-          {activity.level === "amateur" && "Intermédiaire"}
-          {activity.level === "advanced" && "Confirmé"}
-        </p>
-        <p className={`card-tag ${!activity.disabled && "condition-missing"}`}>
-          <img src="/icons/disabled.png" alt="logo disabled" />
-          Handisport
-        </p>
-        <p className={`card-tag ${!activity.locker && "condition-missing"}`}>
-          <img src="/icons/locker.png" alt="logo locker" />
-          Vestiaires
-        </p>
-        <p className={`card-tag ${!activity.shower && "condition-missing"}`}>
-          <img src="/icons/shower.png" alt="logo shower" />
-          Douches
-        </p>
-        <p className={`card-tag ${!activity.toilet && "condition-missing"}`}>
-          <img src="/icons/toilet.png" alt="logo toilet" />
-          Toilettes
-        </p>
-        <p
-          className={`card-tag ${!activity.air_conditioning && "condition-missing"}`}
-        >
-          <img
-            src="/icons/air-conditionning.png"
-            alt="logo air conditionning"
-          />
-          Clim
-        </p>
-      </div>
-      <div className="nb-participant">
-        <p>
-          <img src="/icons/participants.png" alt="logo participants" />
-          {`${activity.nb_participant}/${activity.nb_spots} Participants`}
-        </p>
-        <p>{`${nbAvailableSpots < 0 ? "0" : nbAvailableSpots} ${nbAvailableSpots <= 1 ? "place restante" : "places restantes"}`}</p>
-      </div>
-      <div className="bar">
-        <div
-          className={`progress-bar ${activity.nb_participant >= activity.nb_spots / 2 && "almost-full"} ${nbAvailableSpots === 0 && "full"}`}
-          style={{ "--size": `${widthProgressBar}%` } as React.CSSProperties}
-        >
-          {" "}
+    <>
+      <article className="card">
+        <div className={`card-header ${activity.name}`}>
+          <h2>{activity.name}</h2>
+          <p className={`label-price ${price === 0 ? "free" : "paid"}`}>
+            {price === 0
+              ? "Gratuit"
+              : price % 1 === 0
+                ? `${price} €`
+                : `${price.toFixed(2)} €`}
+          </p>
         </div>
-      </div>
-      <div className="card-footer">
-        <div className="user-organizer">
-          <img src={activity.user_picture} alt="user" />
-          <p>{activity.username}</p>
+        <div className="important-info">
+          <img src="/icons/calendar.png" alt="icon-calendar" />
+          <p>
+            {formattedPlayingAt.charAt(0).toUpperCase() +
+              formattedPlayingAt.slice(1)}
+          </p>
+          <img src="/icons/clock.png" alt="icon-clock" />
+          <p>{activity.playing_time.slice(0, 5).replace(":", "h")}</p>
+          <img src="/icons/pin.png" alt="icon-pin" />
+          <p>{activity.city}</p>
         </div>
-        <button type="button">
-          {nbAvailableSpots === 0 ? (
-            <>
-              <img src="/icons/bell.png" alt="logo alert" />
-            </>
-          ) : (
-            <>Réserver &gt;</>
-          )}
+        <div className="card-tags">
+          <p className="card-tag">
+            {activity.level === "all" && "Tous niveaux"}
+            {activity.level === "beginner" && "Débutant"}
+            {activity.level === "amateur" && "Intermédiaire"}
+            {activity.level === "advanced" && "Confirmé"}
+          </p>
+          <p
+            className={`card-tag ${!activity.disabled && "condition-missing"}`}
+          >
+            <img src="/icons/disabled.png" alt="logo disabled" />
+            Handisport
+          </p>
+          <p className={`card-tag ${!activity.locker && "condition-missing"}`}>
+            <img src="/icons/locker.png" alt="logo locker" />
+            Vestiaires
+          </p>
+          <p className={`card-tag ${!activity.shower && "condition-missing"}`}>
+            <img src="/icons/shower.png" alt="logo shower" />
+            Douches
+          </p>
+          <p className={`card-tag ${!activity.toilet && "condition-missing"}`}>
+            <img src="/icons/toilet.png" alt="logo toilet" />
+            Toilettes
+          </p>
+          <p
+            className={`card-tag ${!activity.air_conditioning && "condition-missing"}`}
+          >
+            <img
+              src="/icons/air-conditionning.png"
+              alt="logo air conditionning"
+            />
+            Clim
+          </p>
+        </div>
+        <div className="nb-participant">
+          <p>
+            <img src="/icons/participants.png" alt="logo participants" />
+            {`${activity.nb_participant}/${activity.nb_spots} Participants`}
+          </p>
+          <p>{`${nbAvailableSpots < 0 ? "0" : nbAvailableSpots} ${nbAvailableSpots <= 1 ? "place restante" : "places restantes"}`}</p>
+        </div>
+        <div className="bar">
+          <div
+            className={`progress-bar ${activity.nb_participant >= activity.nb_spots / 2 && "almost-full"} ${nbAvailableSpots === 0 && "full"}`}
+            style={{ "--size": `${widthProgressBar}%` } as React.CSSProperties}
+          >
+            {" "}
+          </div>
+        </div>
+        {!publicatedRoad && (
+          <>
+            <div className="card-footer">
+              <div className="user-organizer">
+                <img src={activity.user_picture} alt="user" />
+                <p>{activity.username}</p>
+              </div>
+              <button type="button">
+                {nbAvailableSpots === 0 ? (
+                  <>
+                    <img src="/icons/bell.png" alt="logo alert" />
+                  </>
+                ) : (
+                  <>Réserver &gt;</>
+                )}
+              </button>
+            </div>
+            <div className={nbAvailableSpots === 0 ? "activity-full" : ""}>
+              {" "}
+            </div>
+          </>
+        )}
+        <button
+          type="button"
+          className="dropdown-participation"
+          onClick={() => setShowParticipants(!showParticpants)}
+        >
+          {activity.visibility ? "Liste des participants" : "Liste des invités"}
         </button>
-      </div>
-      <div className={nbAvailableSpots === 0 ? "activity-full" : ""}> </div>
-    </article>
+        {showParticpants && (
+          <ParticipantsList id={activity.id} visibility={activity.visibility} />
+        )}
+      </article>
+    </>
   );
 }
 
