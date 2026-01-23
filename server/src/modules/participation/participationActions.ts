3bfd6ec52e8127b3873e333c1fc87d66e4690e1b
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-23 12:27:10 +0100

    Update participationActions.ts

--- server/src/modules/participation/participationActions.ts
+++ server/src/modules/participation/participationActions.ts
@@ -1,6 +1,19 @@
 import type { RequestHandler } from "express";
 import participationRepository from "./participationRepository";
 
+const browseByActivity: RequestHandler = async (req, res, next) => {
+  try {
+    const activityId = Number(req.query.id);
+
+    const participants =
+      await participationRepository.readAllParticipants(activityId);
+
+    res.json(participants);
+  } catch (err) {
+    next(err);
+  }
+};
+
 const add: RequestHandler = async (req, res, next) => {
   try {
     const { userId, activityId, status } = req.body;
@@ -17,4 +30,21 @@ const add: RequestHandler = async (req, res, next) => {
   }
 };
 
-export default { add };
+const edit: RequestHandler = async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    const { status } = req.body;
+
+    const affectedRows = await participationRepository.patch(id, status);
+
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { browseByActivity, add, edit };
