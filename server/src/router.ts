afb041e52f3170a9a322ecac241a499b214a2375
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-23 12:29:04 +0100

    Update router.ts

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import activityActions from "./modules/activity/activityActions";
+import participationActions from "./modules/participation/participationActions";
 import sportAction from "./modules/sport/sportAction";
 import userActions from "./modules/user/userActions";
 
@@ -9,10 +10,14 @@ router.get(
   "/api/publications",
   activityActions.browseActivitiesPublicatedByUser,
 );
-
 router.get("/api/activities", activityActions.browse);
+
 router.get("/api/sport", sportAction.browse);
 
-router.get("/api/participants", userActions.browseParticipants);
+router.get("/api/user", userActions.readUser);
+
+router.get("/api/participants", participationActions.browseByActivity);
+router.patch("/api/participant/:id", participationActions.edit);
+router.post("/api/me/invitation", participationActions.add);
 
 export default router;
