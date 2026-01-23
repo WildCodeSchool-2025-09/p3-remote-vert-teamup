04988c98144f873e1beffc61a4a732fe5b2e98e2
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-23 12:26:48 +0100

    Update participationRepository.ts

--- server/src/modules/participation/participationRepository.ts
+++ server/src/modules/participation/participationRepository.ts
@@ -2,14 +2,32 @@ import databaseClient from "../../../database/client";
 import type { Result, Rows } from "../../../database/client";
 
 class participationRepository {
+  async readAllParticipants(activityId: number) {
+    const [rows] = await databaseClient.query<Rows>(
+      `SELECT participation.id, participation.status, user.id as userId, user.username, user.picture FROM participation
+      JOIN user ON participation.user_id = user.id
+      WHERE participation.activity_id = ?`,
+      [activityId],
+    );
+
+    return rows as Participant[];
+  }
+
   async create(userId: number, activityId: number, status: string) {
     const [result] = await databaseClient.query<Result>(
-      `INSERT INTO participation (user_id, activity_id, status)
-      VALUES (?, ?, ?)`,
+      "INSERT INTO participation (user_id, activity_id, status) VALUES (?, ?, ?)",
       [userId, activityId, status],
     );
 
     return result.insertId;
+  }
+
+  async patch(id: number, status: string) {
+    const [result] = await databaseClient.query<Result>(
+      "UPDATE participation SET status = ? WHERE participation.id = ?",
+      [status, id],
+    );
+    return result.affectedRows;
   }
 }
 
