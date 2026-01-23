68aaf888366fc60f88bca290390edcb3a7c4be48
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-23 12:26:23 +0100

    Update userRepository.ts

--- server/src/modules/user/userRepository.ts
+++ server/src/modules/user/userRepository.ts
@@ -2,19 +2,13 @@ import databaseClient from "../../../database/client";
 import type { Rows } from "../../../database/client";
 
 class userRepository {
-  async readAllParticipants(activityId: number) {
+  async readUserbyEmail(email: string) {
     const [rows] = await databaseClient.query<Rows>(
-      `SELECT user.username, user.picture, demand.status FROM user 
-      JOIN demand ON user.id = demand.user_id
-      WHERE demand.activity_id = ?
-      UNION
-      SELECT user.username, user.picture, participation.status FROM user 
-      JOIN participation ON user.id = participation.user_id
-      WHERE participation.activity_id = ?`,
-      [activityId, activityId],
+      "SELECT user.id, user.username, user.picture FROM user WHERE user.email = ?",
+      [email],
     );
 
-    return rows as Participant[];
+    return rows[0] as User;
   }
 }
 
