6d7e0d72b22d8190114e3125be5c8ee3e0fc5798
Solomon Kalandadze <57320141+SolomonKa@users.noreply.github.com>
2026-01-26 12:39:14 +0100

    cleaned upcoming for further implementation

--- client/src/components/MyActivities/Upcoming.tsx
+++ client/src/components/MyActivities/Upcoming.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router";
-
 export default function Upcoming() {
-  const location = useLocation();
-  const newParticipant = location.state;
-
-  console.log("Navigation state", newParticipant);
-
-  useEffect(() => {}, []);
-
   return (
     <>
       <h1>Upcoming</h1>
