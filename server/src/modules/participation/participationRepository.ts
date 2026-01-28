fe159c31c690ac4ddd26a406970e9b4abea887d2
abijoserah <serahabijo94@gmail.com>
2026-01-28 01:45:19 +0100

    few backend corrections

--- server/src/modules/participation/participationRepository.ts
+++ server/src/modules/participation/participationRepository.ts
@@ -4,10 +4,12 @@ import databaseClient from "../../../database/client";
 class ParticipationRepository {
   async readAll(userId: number) {
     const [rows] = await databaseClient.query<RowDataPacket[]>(
-      `SELECT p.*, a.*, s.name AS sport_name
+      `SELECT a.*, u.username, u.picture AS user_picture, s.name,
+        (SELECT COUNT(*) FROM participation WHERE activity_id = a.id AND status = 'accepted') AS nb_participant
        FROM participation AS p
        JOIN activity AS a ON a.id = p.activity_id
        JOIN sport AS s ON s.id = a.sport_id
+       JOIN user AS u ON u.id = a.user_id
        WHERE p.status = 'inviting' AND p.user_id = ?`,
       [userId],
     );
