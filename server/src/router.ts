c30398393843d89e10358e75be147eb1f44283cc
Lisa Bureau <bureau.lisa03@gmail.com>
2026-01-28 15:39:15 +0100

    temp

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import activityActions from "./modules/activity/activityActions";
 import sportActions from "./modules/sport/sportActions";
-import userActions from "./modules/user/userActions";
+// import userActions from "./modules/user/userActions";
 
 const router = express.Router();
 
 router.get("/api/sports", sportActions.browse);
 router.post("/api/activity", activityActions.add);
 router.get("/api/activities", activityActions.browse);
-router.get("/api/users", userActions.readByEmail);
+// router.get("/api/users", userActions.readByEmail);
 router.get("/api/sport", sportActions.read);
 
 export default router;
