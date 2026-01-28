d1b11405ef24c2b522813dbfe6ecfc096a6e3ab1
abijoserah <serahabijo94@gmail.com>
2026-01-28 19:10:51 +0100

    Added update refused and accepted to participation action

--- server/src/modules/participation/participationActions.ts
+++ server/src/modules/participation/participationActions.ts
@@ -11,4 +11,26 @@ const browse: RequestHandler = async (req, res, next) => {
   }
 };
 
-export default { browse };
+const updateRefused: RequestHandler = async (req, res, next) => {
+  try {
+    const userId = Number(req.body.userId);
+    const activityId = Number(req.body.activityId);
+    await participationRepository.editRefused(userId, activityId);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateAccepted: RequestHandler = async (req, res, next) => {
+  try {
+    const userId = Number(req.body.userId);
+    const activityId = Number(req.body.activityId);
+    await participationRepository.editAccepted(userId, activityId);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { browse, updateRefused, updateAccepted };
