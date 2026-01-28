a0bbf6a4ec7716e6cfaf21782917684f4349efb6
abijoserah <serahabijo94@gmail.com>
2026-01-28 01:14:20 +0100

    modified the router file to add participation

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import activityActions from "./modules/activity/activityActions";
+import participationActions from "./modules/participation/participationActions";
 import sportAction from "./modules/sport/sportAction";
 
 const router = express.Router();
@@ -8,5 +9,6 @@ router.get("/api/sports", sportAction.browse);
 router.post("/api/activity", activityActions.add);
 router.get("/api/activities", activityActions.browse);
 router.get("/api/sport", sportAction.read);
+router.get("/api/participation", participationActions.browse);
 
 export default router;
